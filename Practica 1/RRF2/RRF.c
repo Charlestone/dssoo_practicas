45f5b7543fa4bee612b88b89ed35949c2cc043af
charlestone <carlosdr2332@gmail.com>
2018-03-10 12:24:38 +0100

    RRF
    Creo dos carpetas con dos main distintos para que se pueda ver la ejecución cuando primero hay un hilo con prioridad alta y cuando primero hay un hilo con prioridad baja

--- Practica 1/RRF2/RRF.c
+++ Practica 1/RRF2/RRF.c
@@ -30,6 +30,9 @@ static TCB idle;
 static void idle_function(){
   while(1);
 }
+/* Colas de distintas prioridadesa */
+static struct queue* colaA;
+static struct queue* colaB;
 
 /* Initialize the thread library */
 void init_mythreadlib() {
@@ -59,7 +62,7 @@ void init_mythreadlib() {
   if(getcontext(&t_state[0].run_env) == -1){
     perror("*** ERROR: getcontext in init_thread_lib");
     exit(5);
-  }	
+  } 
 
   for(i=1; i<N; i++){
     t_state[i].state = FREE;
@@ -71,6 +74,13 @@ void init_mythreadlib() {
   /* Initialize network and clock interrupts */
   init_network_interrupt();
   init_interrupt();
+  /* Initialize queue */
+  disable_interrupt();
+  colaA = queue_new();
+  colaB = queue_new();
+  /* Encolamos el idle */
+  enqueue(colaB,&idle);
+  enable_interrupt();
 }
 
 
@@ -89,6 +99,8 @@ int mythread_create (void (*fun_addr)(),int priority)
   }
   t_state[i].state = INIT;
   t_state[i].priority = priority;
+  /* Añadimos la siguiente línea para que cada hilo tenga su rodaja completa al inicializarse*/
+  t_state[i].ticks = QUANTUM_TICKS;
   t_state[i].function = fun_addr;
   t_state[i].run_env.uc_stack.ss_sp = (void *)(malloc(STACKSIZE));
   if(t_state[i].run_env.uc_stack.ss_sp == NULL){
@@ -99,6 +111,21 @@ int mythread_create (void (*fun_addr)(),int priority)
   t_state[i].run_env.uc_stack.ss_size = STACKSIZE;
   t_state[i].run_env.uc_stack.ss_flags = 0;
   makecontext(&t_state[i].run_env, fun_addr, 1); 
+  /* Encolamos los hilo a medida que se van creando según su prioridad */
+  if (priority == 1) {
+    /* Si el hilo es de alta prioridad */
+    enqueue(colaA, &t_state[i]);
+    /* Se comprueba si el hilo ejecutandose es de baja prioridad, para llamar al planificador */
+    if (running->priority == 0)
+     {
+       TCB* new = scheduler();   
+      activator(new);
+     } 
+    
+  } else {
+    enqueue(colaB, &t_state[i]);
+  }
+  
   return i;
 } /****** End my_thread_create() ******/
 
@@ -116,9 +143,9 @@ void network_interrupt(int sig)
 
 /* Free terminated thread and exits */
 void mythread_exit() {
-  int tid = mythread_gettid();	
+  int tid = mythread_gettid();  
 
-  printf("*** THREAD %d FINISHED\n", tid);	
+  printf("*** THREAD %d FINISHED\n", tid);  
   t_state[tid].state = FREE;
   free(t_state[tid].run_env.uc_stack.ss_sp); 
 
@@ -128,13 +155,13 @@ void mythread_exit() {
 
 /* Sets the priority of the calling thread */
 void mythread_setpriority(int priority) {
-  int tid = mythread_gettid();	
+  int tid = mythread_gettid();  
   t_state[tid].priority = priority;
 }
 
 /* Returns the priority of the calling thread */
 int mythread_getpriority(int priority) {
-  int tid = mythread_gettid();	
+  int tid = mythread_gettid();  
   return t_state[tid].priority;
 }
 
@@ -146,16 +173,23 @@ int mythread_gettid(){
 }
 
 
-/* FIFO para alta prioridad, RR para baja*/
+/* RR sin prioridad */
 TCB* scheduler(){
-  int i;
-  for(i=0; i<N; i++){
-    if (t_state[i].state == INIT) {
-        current = i;
-	return &t_state[i];
+  disable_interrupt();
+  TCB* aux;
+  if (!queue_empty(colaA))
+  {
+    aux = dequeue(colaA);
+  } else {
+    aux = dequeue(colaB);
+    if(aux->state == 3){
+      enqueue(colaB, aux);
+      aux = dequeue(colaB);
     }
   }
-  printf("mythread_free: No thread in the system\nExiting...\n");	
+  enable_interrupt();
+  return aux;
+  printf("mythread_free: No thread in the system\nExiting...\n"); 
   exit(1); 
 }
 
@@ -163,13 +197,47 @@ TCB* scheduler(){
 /* Timer interrupt  */
 void timer_interrupt(int sig)
 {
+  /* Si el hilo es de prioridad baja, se sigue el Round Robin*/
+  if(running->priority == 0){
+    running->ticks--;
+    /* Comprobamos si el hilo en ejecución ha terminado su cuanto */
+    if(running->ticks == 0) {
+      running->ticks = QUANTUM_TICKS;
+      TCB* aux = scheduler();
+      activator(aux);
+    }
+  }
+  
 } 
 
 /* Activator */
 void activator(TCB* next){
-  setcontext (&(next->run_env));
-  printf("mythread_free: After setcontext, should never get here!!...\n");	
+  TCB* prevrunning = running;
+  running = next;
+  current = next->tid;
+  /* Se comprueba si el hilo que se va a ejecutar es el idle */
+  if(running->state == 3){
+    printf("*** FINISH\n");
+    /* Se debería salir del programa */
+    exit(0);
+  }
+  /* Comprobamos si el hilo que va a ser expulsado ha terminado su ejecución */
+  if(!prevrunning->state) {
+    printf("*** THREAD %i TERMINATED: SETCONTEXT OF %i\n", prevrunning->tid,running->tid);
+    setcontext(&(next->run_env));
+    printf("mythread_free: After setcontext, should never get here!!...\n");  
+  }
+  disable_interrupt();
+  /* Si el hilo que va a salir no ha terminado su ejecución, se le encola de nuevo */
+  enqueue(colaB, prevrunning);
+  /* Solo encolaremos de nuevo los hilos que sean de baja prioridad, porque los de alta siguen un FIFO */
+  enable_interrupt();
+  if (prevrunning->priority == 0 && running->priority == 1)
+  {
+    printf("*** THREAD %i PREEMTED: SETCONTEXT OF %i\n", prevrunning->tid,running->tid);
+  } else {
+    printf("*** SWAPCONTEXT FROM %i TO %i\n", prevrunning->tid,running->tid);
+  }
+  swapcontext(&(prevrunning->run_env),&(next->run_env));
+  //printf("mythread_free: After setcontext, should never get here!!...\n");  
 }
-
-
-
