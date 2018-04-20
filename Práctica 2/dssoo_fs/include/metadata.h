9a3da8a61d79739b56f1b4cd90e760604caf1d2b
charlestone <carlosdr2332@gmail.com>
2018-04-20 21:56:02 +0200

    metadata.h
    Añado comentarios

--- Práctica 2/dssoo_fs/include/metadata.h
+++ Práctica 2/dssoo_fs/include/metadata.h
@@ -13,12 +13,13 @@ static inline void bitmap_setbit(char *bitmap_, int i_, int val_) { /*El código
   else
     bitmap_[(i_ >> 3)] &= ~(1 << (i_ & 0x07));
 } 
-/* Estructura para el superbloque. Ocupa 5208 bits, 651 bytes. */
+/* Estructura para el superbloque. Los datos útiles ocupan 5208 bits, 651 bytes. El superbloque completo ocupa 2048 bytes (1 bloque) */
 typedef struct {
 	unsigned short numBloquesDatos; /* Número de bloques de datos en el disp. Lo ponemos como short porque no hace falta que sea un ing. Ocupa 5 * 2^10*/
-	char bmapai[5];
-	char bmapab[640]; 
+	char bmapai[5]; //Mapa de bits de los inodos
+	char bmapab[640]; //Mapa de bits de los bloques de datos
 	unsigned int tamDispositivo; /* Tamaño total del disp. (en bytes) Numero de bloques * 2048 (tamaño de bloque) */
+	char relleno[1397]; //Relleno del bloque
 } superbloque;
 /* Estructura para cada inodo. Ocupa 304 bits, 38 bytes. */
 typedef struct {
