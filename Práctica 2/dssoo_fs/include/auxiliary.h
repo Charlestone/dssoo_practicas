ae2823832520a1701c19f787c67a42f5f88b295c
charlestone <carlosdr2332@gmail.com>
2018-04-21 19:23:45 +0200

    auxiliary.h
    Añado las funciones ialloc, alloc, namei, ifree y bfree

--- Práctica 2/dssoo_fs/include/auxiliary.h
+++ Práctica 2/dssoo_fs/include/auxiliary.h
@@ -5,3 +5,33 @@
  * @brief 	Headers for the auxiliary functions required by filesystem.c.
  * @date	01/03/2017
  */
+
+/*
+ * @brief 	Devuelve el primer inodo libre en el sistema de ficheros.
+ * @return 	Número del primer inodo libre, -1 si no hay inodos libres.
+ */
+int ialloc(void);
+
+/*
+ * @brief 	Devuelve el primer bloque libre en el sistema de ficheros
+ * @return 	Número del primer bloque libre, -1 en otro caso.
+ */
+int alloc(void);
+
+/*
+ * @brief 	Comprueba si existe un inodo con cierto nombre
+ * @return 	El número del inodo, -1 si no existe un inodo con ese nombre.
+ */
+int namei(char *fileName);
+
+/*
+ * @brief 	Libera un inodo
+ * @return 	0 si se libera correctamente, -1 en otro caso.
+ */
+int ifree(int inodo);
+
+/*
+ * @brief 	Libera un bloque
+ * @return 	0 si se libera correctamente, -1 en otro caso.
+ */
+int bfree(int bloque);
