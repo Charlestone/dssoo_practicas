e141b23f275a46a3f56875db65735a23795e6180
charlestone <carlosdr2332@gmail.com>
2018-04-21 19:24:51 +0200

    metadata.h
    Añado el campo numInodos al superbloque porque nos sobra sitio y así queda más limpio a la hora de codificar (aunque se puede quitar).

--- Práctica 2/dssoo_fs/include/metadata.h
+++ Práctica 2/dssoo_fs/include/metadata.h
@@ -15,17 +15,21 @@ static inline void bitmap_setbit(char *bitmap_, int i_, int val_) { /*El código
 } 
 
 #include <stdint.h>				//Header para utilizar uint
-/* Estructura para el superbloque. Los datos útiles ocupan 5208 bits, 651 bytes. El superbloque completo ocupa 2048 bytes (1 bloque) */
+
+/* Estructura para el superbloque. Los datos útiles ocupan 5216 bits, 652 bytes. El superbloque completo ocupa 2048 bytes (1 bloque) */
 typedef struct {
-	uint16_t numBloquesDatos; /* Número de bloques de datos en el disp. Lo ponemos como short porque no hace falta que sea un ing. Ocupa 5 * 2^10*/
-	char bmapai[5]; //Mapa de bits de los inodos
-	char bmapab[640]; //Mapa de bits de los bloques de datos
-	uint32_t tamDispositivo; /* Tamaño total del disp. (en bytes) Numero de bloques * 2048 (tamaño de bloque) */
-	char relleno[1397]; //Relleno del bloque
+	uint8_t numInodos; 			/* Número de inodos totales en el dispositivo */
+	uint16_t numBloquesDatos; 	/* Número de bloques de datos totales en el disp. Lo ponemos como short porque no hace falta que sea un ing. Ocupa 5 * 2^10*/
+	char bmapai[5]; 			//Mapa de bits de los inodos
+	char bmapab[640]; 			//Mapa de bits de los bloques de datos
+	uint32_t tamDispositivo; 	/* Tamaño total del disp. (en bytes) Numero de bloques * 2048 (tamaño de bloque) */
+	char relleno[1396]; 		//Relleno del bloque
 } superbloque;
-/* Estructura para cada inodo. Ocupa 304 bits, 38 bytes. */
+
+/* Estructura para cada inodo. Ocupa 336 bits, 42 bytes. */
 typedef struct {
-	char nombre[32]; /* Nombre del fichero/directorio asociado cuya longitud máxima es de 32 caracteres */
-	uint32_t tamanyo; /* Tamaño actual del fichero en bytes */
-	uint16_t bloqueIndirecto; /* Indica el bloque en el que se encuentra el índice de bloques de datos que conforman el fichero*/
+	char nombre[32]; 			/* Nombre del fichero/directorio asociado cuya longitud máxima es de 32 caracteres */
+	uint32_t tamanyo; 			/* Tamaño actual del fichero en bytes */
+	uint32_t CRC;				/* CRC para comprobar la integridad del fichero */
+	uint16_t bloqueIndirecto; 	/* Indica el bloque en el que se encuentra el índice de bloques de datos que conforman el fichero*/
 } inodo;
